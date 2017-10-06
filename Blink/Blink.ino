feced5600f01bbe11dd2cdc1bb21778bbd3f0678
DcloudGit <dbernardi@hydor.com>
2017-10-06 14:34:02 +0200

    Change D
    Change from 4 to 7

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -30,6 +30,6 @@ void loop() {
   //digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   //delay(5000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(4000);              // wait for a second
+  delay(7000);              // wait for a second
 
 }
